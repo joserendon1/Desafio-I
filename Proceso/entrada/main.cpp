222fa6314adc4d1086272cf7aad1b5cd3dfa71c0
Juan Jose Rendon Gonzalez <jose.rendon1@udea.edu.co>
2025-09-24 15:07:48 -0500

    En el programa entrada se soluciona el problema que se presentaba cuando el usuario ingresaba un numero que mayor a 9

--- Proceso/entrada/main.cpp
+++ Proceso/entrada/main.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
 
-int main()
-{
-    char n;
-    char nombreArchivo[] = "EncriptadoX.txt";
-    char nombrePista[] = "pistaX.txt";
-    std::cin >> n;
-    nombreArchivo[10] = n ;
-    nombrePista[5] = n;
-    std::cout << nombreArchivo << std::endl;
-    std::cout << nombrePista << std::endl;
+int main() {
+    char numero[10];
+    std::cout << "Ingrese el numero: ";
+    std::cin >> numero;
+
+    char nombreArchivo[50];
+    char nombrePista[50];
+
+    int i = 0;
+    const char prefijoArchivo[] = "Encriptado";
+    const char sufijo[] = ".txt";
+
+    for (int j = 0; prefijoArchivo[j] != '\0'; j++) {
+        nombreArchivo[i++] = prefijoArchivo[j];
+    }
+
+    for (int j = 0; numero[j] != '\0'; j++) {
+        nombreArchivo[i++] = numero[j];
+    }
+
+    for (int j = 0; sufijo[j] != '\0'; j++) {
+        nombreArchivo[i++] = sufijo[j];
+    }
+    nombreArchivo[i] = '\0';
+
+    i = 0;
+    const char prefijoPista[] = "pista";
+
+    for (int j = 0; prefijoPista[j] != '\0'; j++) {
+        nombrePista[i++] = prefijoPista[j];
+    }
+
+    for (int j = 0; numero[j] != '\0'; j++) {
+        nombrePista[i++] = numero[j];
+    }
+
+    for (int j = 0; sufijo[j] != '\0'; j++) {
+        nombrePista[i++] = sufijo[j];
+    }
+    nombrePista[i] = '\0';
+
+    std::cout << "Archivo: " << nombreArchivo << std::endl;
+    std::cout << "Pista: " << nombrePista << std::endl;
 
     return 0;
 }
